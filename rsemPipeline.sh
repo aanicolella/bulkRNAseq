24569629498e7d839c271bfcc5a5218fc8c0f847
Ally Nicolella <31012168+aanicolella@users.noreply.github.com>
2021-04-23 20:14:18 -0400

    script for running RSEM alignment

---
+++ rsemPipeline.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+
+#$ -cwd
+#$ -q broad
+#$ -l h_vmem=100g
+#$ -e parentDir/sample/sample.err
+#$ -o parentDir/sample/sample.out
+#$ -l h_rt=48:00:00
+
+reuse -q UGER
+reuse -q Rsem
+reuse -q Bowtie2
+reuse -q Samtools
+reuse -q R-3.5
+
+rsem-calculate-expression -p 8  --bowtie2 --paired-end --estimate-rspd --append-names --sort-bam-by-coordinate parentDir/sample/sample.unmapped.1.fastq.gz parentDir/sample/sample.unmapped.2.fastq.gz homeDir/references/mouse_gencode/mouse_gencode parentDir/sample/sample
+
+
+
