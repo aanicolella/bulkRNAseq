71b5c3fba12d215f908de35a3168e031fa729df1
Ally Nicolella <31012168+aanicolella@users.noreply.github.com>
2021-07-29 13:11:31 -0400

    Script for downloading all run data from GP

---
+++ download.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+#$ -l h_vmem=64g
+#$ -o /path/to/output/dir
+#$ -l h_rt=06:00:00
+
+wget --tries=10 --continue --mirror --user insertUserName --password insertPassword --no-check-certificate --cut-dirs 3 -P /path/to/output/rawData https://get.broadinstitute.org/pkgs/runNumber/
+
+
