1ad7c0b533d209944f6ad6b9f5c655431573a551
Ally Nicolella <31012168+aanicolella@users.noreply.github.com>
2023-06-29 13:12:48 -0400

    Update multipleGeno_DESeq.R

--- multipleGeno_DESeq.R
+++ multipleGeno_DESeq.R
@@ -1,30 +1,40 @@
 # Variables to replace
     # EXP: name of experiment, usually the gene of interest
     # REGION: brain region you are studying
-    # X, Y, Z: number of replicates for HT, KO, WT
+    # X, Y: number of replicates for each genotype
+
 
 # load in libraries, path to data directory, and list of sample names.
     # i.e.
         # filePath <- "/my/data/dir/"
-        # sampleNames <- c("HT1", "HT2", "HT3", "KO1", "KO2", "KO3", "WT1", "WT2", "WT3")
-        
+        # sampleNames <- c("KO1", "KO2", "KO3", "WT1", "WT2", "WT3")
+
 library("DESeq2")
 library("tximport")
-filePath <- "/path/to/data/REGION/"
-sampleNames <- c()
 
+# File paths and sample names
+filePath <- "/parentDir/REGION/"
+sampleNames <- c()
 # Set up metadata
     # model = genotype 
     # sample = individual sample name
-colData = data.frame(model=c(rep(c("EXP_HT"), X), rep(c("EXP_KO"), Y), rep(c("EXP_WT"), Z)),sample=sampleNames)
+colData = data.frame(model=c(rep(c("EXP_HT"), X), rep(c("EXP_KO"), Y), rep(c("EXP_WT"), Z)),sample=colData$sample)
 colData$model = relevel(colData$model, "EXP_WT")
-files <- file.path(filePath, paste0(sampleNames, "/", sampleNames, ".genes.results"))
-names(files) <- sampleNames
-rsem.in <- tximport(files, type="rsem", txIn=FALSE, txOut=FALSE)
+
+# Read in files for Salmon alignment
+files <- paste0(filePath, colData$sample, "/Salmon_out/quant.sf")
+names(files) <- colData$sample
+tx2gene = read.table(paste0(filePath, colData$sample[1], "/Salmon_out/trans2gene.txt"))
+txi.salmon = tximport(files, type = "salmon", tx2gene = tx2gene)
+
+# Read in files for RSEM alignment
+#files <- file.path(filePath, paste0(colData$sample, "/", colData$sample, ".genes.results"))
+#names(files) <- sampleNames
+#rsem.in <- tximport(files, type="rsem", txIn=FALSE, txOut=FALSE)
 
 # Optional: find genes below expression threshhold. Create 'NA' data frame to re-add lowly expressed 
 # genes for easier comparison between analyses.
-drop <- rsem.in
+drop <- txi.salmon
 drop$abundance <-
     drop$abundance[apply(drop$length,
                              1,
@@ -42,108 +52,63 @@ dropped <- drop$counts
 dropRes <- data.frame(row.names=make.unique(row.names(drop$counts)), matrix(ncol=6, nrow=dim(drop$counts)[1]))
 cols = c("baseMean", "log2FoldChange", "lfcSE", "stat", "pvalue", "padj")
 colnames(dropRes) <- cols
-EXPName1 <- data.frame(do.call('rbind', strsplit(as.character(row.names(dropRes)), '_', fixed=TRUE)))
-EXPName1 <- EXPName1["X2"]               
-dropRes$symbol <- EXPName1$X2
+geneName1 <- data.frame(do.call('rbind', strsplit(as.character(row.names(dropRes)), '_', fixed=TRUE)))
+geneName1 <- geneName1["X2"]               
+dropRes$symbol <- geneName1$X2
 
 # Filter lowly expressed genes found above out of the main dataset for the DE analysis
-rsem.in$abundance <-
-  rsem.in$abundance[apply(rsem.in$length,
+txi.salmon$abundance <-
+  txi.salmon$abundance[apply(txi.salmon$length,
                              1,
                              function(row) all(row > 5 )),]
-rsem.in$counts <-
-  rsem.in$counts[apply(rsem.in$length,
+txi.salmon$counts <-
+  txi.salmon$counts[apply(txi.salmon$length,
                              1,
                              function(row) all(row > 5 )),]
-rsem.in$length <-
-  rsem.in$length[apply(rsem.in$length,
+txi.salmon$length <-
+  txi.salmon$length[apply(txi.salmon$length,
                              1,
                              function(row) all(row > 5 )),]
-
+                       
 # Convert data structure to proper format and run DESeq
-dds <- DESeqDataSetFromTximport(rsem.in, colData, ~model)
+dds <- DESeqDataSetFromTximport(txi.salmon, colData, ~model)
 dds <- DESeq(dds)
 check <- estimateSizeFactors(dds)
 norm <- counts(check, normalized=TRUE)
                        
 # Combine normalized count dataframe with the 'NA' dataframe and save
 all <- rbind(norm, dropped)                                          
-EXPName <- data.frame(do.call('rbind', strsplit(as.character(row.names(all)), '_', fixed=TRUE)))
-EXPName <- EXPName["X2"]               
-row.names(all) <- EXPName$X2
+geneName <- data.frame(do.call('rbind', strsplit(as.character(row.names(all)), '_', fixed=TRUE)))
+geneName <- geneName["X2"]               
+row.names(all) <- geneName$X2
 all <- all[order(row.names(all)),]
 head(all)
-write.csv(all, "EXP_REGION_normalizedCounts.csv", row.names=TRUE)
+write.csv(all, "EXP_normalizedCounts.csv", row.names=TRUE)
                        
-# Plot pca using different metadata conditions to see relationships between samples
-pdf(file="EXP_REGION_pca.pdf")
+# Plot pca using different metadata conditions to see relationships between samples                   
+pdf(file="EXP_pca.pdf")
 vsdata <- vst(dds, blind=FALSE)
 plotPCA(vsdata, intgroup="sample")
 dev.off()
-pdf(file="EXP_REGION_modelpca.pdf")
+pdf(file="EXP_modelpca.pdf")
 vsdata <- vst(dds, blind=FALSE)
 plotPCA(vsdata, intgroup="model")
 dev.off()
-
-# Extract DE results from different genotype comparisions
-    # Currently set up such that the LFC directionality is in relation to the changes in the first 
-    # genotype listed: i.e. + lfc == upregulation in that geno, - lfc == downregulation
-    # KOHT relationship is optional, sometimes interesting, primarily for QC purposes
-resHT <- results(dds, contrast=c("model", "EXP_HT","EXP_WT"))
-resKO <- results(dds, contrast=c("model", "EXP_KO","EXP_WT"))
-resKOHT <- results(dds, contrast=c("model", "EXP_KO","EXP_HT"))
+                       
+# Extract DE results 
+    # Currently set up such that the LFC directionality is in relation to the mutant: i.e. + lfc == 
+    # upregulation in the mutant, - lfc == downregulation
+res <- results(dds)
 head(results(dds, tidy=TRUE), n=6)
 summary(dds)
 
-# Combine unscaled HTvsWT results and 'NA' dropped gene dataframe then write to .csv
-EXPNameHT <- data.frame(do.call('rbind', strsplit(as.character(row.names(resHT)), '_', fixed=TRUE)))
-EXPNameHT <- EXPNameHT["X2"]               
-resHT$symbol <- EXPNameHT$X2
-resMergeHT <- rbind(resHT, dropRes)
-dim(resMergeHT)
-resMergeHT <- resMergeHT[order(resMergeHT$pvalue),]
-write.csv(resMergeHT, "EXP_REGION_HTWT-DESeq2_allgenes.csv")
-# Scale HTvsWT results using lfcShrink(): https://rdrr.io/bioc/DESeq2/man/lfcShrink.html
+# Scale results using lfcShrink(): https://rdrr.io/bioc/DESeq2/man/lfcShrink.html
    # Combine post-shrinkage results with 'NA' dropped dataframe then write to .csv
-resHT <- lfcShrink(dds=dds, contrast=c("model", "EXP_HT", "EXP_WT"), res=resHT, type="normal")
-EXPNameHT <- data.frame(do.call('rbind', strsplit(as.character(row.names(resHT)), '_', fixed=TRUE)))
-EXPNameHT <- EXPNameHT["X2"]               
-resHT$symbol <- EXPNameHT$X2
-resMergeHT <- rbind(resHT, dropRes)
-dim(resMergeHT)
-resMergeHT <- resMergeHT[order(resMergeHT$pvalue),]
-write.csv(resMergeHT, "EXP_REGION_HTWT-DESeq2_LFCshrink.csv")
-
-# Repeat above for KOvsWT
-EXPNameKO <- data.frame(do.call('rbind', strsplit(as.character(row.names(resKO)), '_', fixed=TRUE)))
-EXPNameKO <- EXPNameKO["X2"]               
-resKO$symbol <- EXPNameKO$X2
-resMergeKO <- rbind(resKO, dropRes)
-dim(resMergeKO)
-resMergeKO <- resMergeKO[order(resMergeKO$pvalue),]
-write.csv(resMergeKO, "EXP_REGION_KOWT-DESeq2_allgenes.csv")
-resKO <- lfcShrink(dds=dds, contrast=c("model", "EXP_KO", "EXP_WT"), res=resKO, type="normal")
-EXPNameKO <- data.frame(do.call('rbind', strsplit(as.character(row.names(resKO)), '_', fixed=TRUE)))
-EXPNameKO <- EXPNameKO["X2"]               
-resKO$symbol <- EXPNameKO$X2
-resMergeKO <- rbind(resKO, dropRes)
-dim(resMergeKO)
-resMergeKO <- resMergeKO[order(resMergeKO$pvalue),]
-write.csv(resMergeKO, "EXP_REGION_KOWT-DESeq2_LFCshrink.csv")
-
-# Repeat above for KOvsHT
-EXPNameKOHT <- data.frame(do.call('rbind', strsplit(as.character(row.names(resKOHT)), '_', fixed=TRUE)))
-EXPNameKOHT <- EXPNameKOHT["X2"]               
-resKOHT$symbol <- EXPNameKOHT$X2
-resMergeKOHT <- rbind(resKOHT, dropRes)
-dim(resMergeKOHT)
-resMergeKOHT <- resMergeKOHT[order(resMergeKOHT$pvalue),]
-write.csv(resMergeKOHT, "EXP_REGION_KOHTWT-DESeq2_allgenes.csv")
-resKOHT <- lfcShrink(dds=dds, contrast=c("model", "EXP_KO", "EXP_HT"), res=resKOHT, type="normal")
-EXPNameKOHT <- data.frame(do.call('rbind', strsplit(as.character(row.names(resKOHT)), '_', fixed=TRUE)))
-EXPNameKOHT <- EXPNameKOHT["X2"]               
-resKOHT$symbol <- EXPNameKOHT$X2
-resMergeKOHT <- rbind(resKOHT, dropRes)
-dim(resMergeKOHT)
-resMergeKOHT <- resMergeKOHT[order(resMergeKOHT$pvalue),]
-write.csv(resMergeKOHT, "EXP_REGION_KOHT-DESeq2_LFCshrink.csv")
+res <- lfcShrink(dds=dds, coef=2, res=res, type="normal")
+geneName <- data.frame(do.call('rbind', strsplit(as.character(row.names(res)), '_', fixed=TRUE)))
+geneName <- geneName["X2"]               
+res$symbol <- geneName$X2
+resMerge <- rbind(res, dropRes)
+dim(resMerge)
+resMerge <- resMerge[order(resMerge$pvalue),]
+write.csv(resMerge, "EXP-DESeq2_allGenes.csv")
